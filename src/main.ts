42a79fe201370c654a6b3e68d579316e6d528292
William Etheredge <me@wetheredge.com>
2023-03-22 15:40:26 -0500

    Replace `SimpleParser` with `init` function

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,17 @@ import { Parser } from './parser';
 import { Wasm } from './wasm';
 
 export * from './common';
+export { Parser };
 
-export class SimpleParser extends Parser {
-	static async init(): Promise<SimpleParser> {
-		const decoded = atob(encodedWasm);
+export async function init(): Promise<Parser> {
+	const decoded = atob(encodedWasm);
 
-		const bytes = new Uint8Array(decoded.length);
-		for (let i = 0; i < decoded.length; i++) {
-			bytes[i] = decoded.charCodeAt(i);
-		}
-
-		const wasmModule = await WebAssembly.compile(bytes);
-		const wasm = await Wasm.init(wasmModule);
-		return new SimpleParser(wasm);
+	const bytes = new Uint8Array(decoded.length);
+	for (let i = 0; i < decoded.length; i++) {
+		bytes[i] = decoded.charCodeAt(i);
 	}
 
-	private constructor(wasm: Wasm) {
-		if (!(wasm instanceof Wasm)) {
-			throw new Error('create a SimpleParser using its init() method, not new');
-		}
-
-		super(wasm);
-	}
+	const wasmModule = await WebAssembly.compile(bytes);
+	const wasm = await Wasm.init(wasmModule);
+	return new Parser(wasm);
 }
